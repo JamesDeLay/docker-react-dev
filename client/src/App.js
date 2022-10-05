046953554125875f20718cc190fc87cfadd9a86c
James DeLay <31267952+JamesDeLay@users.noreply.github.com>
2022-10-05 19:33:12 -0400

    scaffold out UI, adding docker dev

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  // const doFetchAPI = async () => {
+  //   try {
+  //     let res = await fetch('/')
+  //   } catch (error) {
+      
+  //   }
+  // }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+        <button className="btn">
+          API Call
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
